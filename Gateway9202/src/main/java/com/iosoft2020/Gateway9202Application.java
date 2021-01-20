3785ebbf0b5b2fa2b82b4c67d2212cf652fd31c2
root <liwei.duan@hotmail.com>
2021-01-20 12:53:39 +0900

    create spring cloud gateway

---
+++ Gateway9202/src/main/java/com/iosoft2020/Gateway9202Application.java
@@ -0,0 +1,13 @@
+package com.iosoft2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gateway9202Application {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Gateway9202Application.class, args);
+	}
+
+}
